a294210429eadc2de0699e78dacd233c04fce00c
SouryaShreyash <souryashreyash2005@gmail.com>
2025-07-27 00:49:42 +0530

    new sort added

---
+++ sort/cyclicsort.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class cyclicsort {
+  public static void cyclicsort(int[] arr) {
+    int i = 0;
+    while (i < arr.length) {
+      int correct = arr[i] - 1;
+      if (arr[i] != arr[correct]) {
+        swap(arr, i, correct);
+      } else {
+        i++;
+      }
+    }
+  }
+
+  public static void swap(int[] arr, int first, int second) {
+    int temp = arr[first];
+    arr[first] = arr[second];
+    arr[second] = temp;
+  }
+
+  public static void main(String[] args) {
+    int[] arr = { 3, 1, 2, 5, 4 };
+    cyclicsort(arr);
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+  }
+}
