04c946fa061df5b7dbdf24300270b759e267857a
SouryaShreyash <souryashreyash2005@gmail.com>
2025-07-27 22:33:28 +0530

    resolved merge conflict

--- sort/findthemissingno.java
+++ sort/findthemissingno.java
@@ -1,6 +1,7 @@
-//find the missing no.in an array
-public class findthemissingno{
-public static int findthemissingno(int[] arr) {
+import java.util.*;
+//https://leetcode.com/problems/missing-number/description/
+public class findthemissingno {
+  public static int missingno(int[] arr) {
     int i = 0;
     while (i < arr.length) {
       int correct = arr[i];
@@ -10,23 +11,21 @@ public static int findthemissingno(int[] arr) {
         i++;
       }
     }
-    //case 1:search for missing number(any middle number)
     for(int index=0;index<arr.length;index++){
-       if(arr[index]!=index){
-           return index;
-       } 
+      if(arr[index]!=index){
+        return index;
+      }
     }
-     //cse 2:if the last no. is missing
     return arr.length;
   }
-  public static void swap(int[] arr, int first, int second) {
+   public static void swap(int[] arr, int first, int second) {
     int temp = arr[first];
     arr[first] = arr[second];
     arr[second] = temp;
   }
   public static void main(String[] args) {
-    int[] arr = { 3,0,2,1 };
-    int ans=findthemissingno(arr);
+    int arr[]={4,2,0,1};
+    int ans=missingno(arr);
     System.out.println(ans);
   }
 }
