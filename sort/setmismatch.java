205e510f9d195c98a16549e90c64b48efa5a815c
SouryaShreyash <souryashreyash2005@gmail.com>
2025-07-28 19:28:09 +0530

    leetcode questions added

---
+++ sort/setmismatch.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class setmismatch {
+  public static int[] findErrorNums(int[] nums) {
+        int i=0;
+      while(i<nums.length){
+        int correct=nums[i]-1;
+        if(nums[i]!=nums[correct]){
+            swap(nums,i,correct);
+        }
+        else{
+          i++;
+        }
+      }
+      for(int index=0;index<nums.length;index++){
+        if(nums[index]!=index+1){
+          return new int[] {nums[index],index+1};
+        }
+      }
+      return new int[] {-1,-1};
+    }
+    public static void swap(int[] arr, int first, int second) {
+    int temp = arr[first];
+    arr[first] = arr[second];
+    arr[second] = temp;
+  }
+  public static void main(String[] args) {
+    int arr[]={1,2,2,4};
+    int[] ans=findErrorNums(arr);
+    System.out.println(Arrays.toString(ans));
+  }
+}
