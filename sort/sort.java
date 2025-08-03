e2f52993a5c9e55a7103efd52ffff16d587c89bb
SouryaShreyash <souryashreyash2005@gmail.com>
2025-08-03 22:52:41 +0530

    same as before

--- sort/sort.java
+++ sort/sort.java
@@ -42,8 +42,6 @@ public class sort {
 
         for (int val : arr) {
             System.out.print(val + " ");
-            System.out.println("hello");
-            System.out.println("hello2");
         }
     }
 }
